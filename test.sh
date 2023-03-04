59ec67a4250e5b4721b131b9c774680e518a3d11
Elias Batek <desisma@heidel.beer>
2023-03-04 16:58:21 +0100

    Add microframework-quickstart example to test script

--- test.sh
+++ test.sh
@@ -1,14 +1,17 @@
 #!/bin/dash
 cd $(dirname "$0")
 
-dub test :message || exit
-dub test :server || exit
-dub test :microframework || exit
+dub test :message || exit 1
+dub test :server || exit 1
+dub test :microframework || exit 1
 
 cd examples
 
 cd server
-dub build --build=plain || exit
+dub build --build=plain || exit 1
 
 cd ../microframework
-dub build --build=plain || exit
+dub build --build=plain || exit 1
+
+cd ../microframework-quickstart 1
+dub build --build=plain || exit 1
